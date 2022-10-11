1ee45f2361a8a81e65f13d73e7fcc201f9aefa41
tetianadac <datsenkotetiana027@gmail.com>
2022-10-11 17:18:23 +0300

    task first

---
+++ js/firsttask.js
@@ -0,0 +1,22 @@
+`use strict`;
+// first task
+const menuItemsByTagNumber = categories.querySelectorAll(`.item`);
+console.log(`Number of categories: ${menuItemsByTagNumber.length}`);
+
+const items = document.querySelectorAll(`.item`);
+Array.prototype.forEach.call(items, (element) => {
+    const title = element.querySelector(`h2`).innerHTML;
+    const itemsLength = element.querySelectorAll(`li`).length;
+    console.log(`Category: ${title} / Elements: ${itemsLength}`);
+});
+// second task
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+
+console.log(ingredients)
