3e087dfa573f6d6db4d7e035093c22f287e10342
tetianadac <datsenkotetiana027@gmail.com>
2022-10-14 19:36:48 +0300

    HOMEWORK

--- js/sixthtask.js
+++ js/sixthtask.js
@@ -1,20 +1,20 @@
 `use strict`
 
-// let inputVal = document.getElementById("validation-input");
+const inputVal = document.getElementById("validation-input");
+const totalLenght = inputVal.getAttribute("data-length");
+const intTotallenght = parseInt(totalLenght, 10);
+inputVal.addEventListener("focus", function( event ) {
+    event.target.style.outline = "none";
+}, true);
+inputVal.addEventListener("blur", () => {
+    if (inputVal.value.length === intTotallenght) {
+        inputVal.classList.add("valid");
+        inputVal.classList.remove("invalid");
+    } if (inputVal.value.length === 0) {
+        inputVal.classList.remove("valid");
+        inputVal.classList.remove("invalid");
+    } if (inputVal.value.length !== intTotallenght && inputVal.value.length !== 0) {
+        inputVal.classList.add("invalid");
 
-// let totalLenght = inputVal.getAttribute("data-length");
-// let intTotallenght = parseInt(totalLenght, 12);
-
-// inputVal.oninput = function() {
-//   if (inputVal.value.length === intTotallenght) {
-//     inputVal.classList.remove("invalid");
-//     inputVal.classList.add("valid");
-//   }
-//   if (inputVal.value.length === 0) {
-//     inputVal.classList.remove("valid");
-//     inputVal.classList.remove("invalid");
-//   }
-//   if (inputVal.value.length !== intTotallenght && inputVal.value.length !== 0) {
-//     inputVal.classList.add("invalid");
-//   }
-// };
+    }
+});
