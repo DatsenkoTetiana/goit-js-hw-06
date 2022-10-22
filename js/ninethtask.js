f107ce19cfae2b1a9fed7cd4c63ec054c232cfce
tetianadac <datsenkotetiana027@gmail.com>
2022-10-22 19:45:15 +0300

    nineth

--- js/ninethtask.js
+++ js/ninethtask.js
@@ -1,8 +1,15 @@
 `use strict`
+const button = document.querySelector(".change-color")
+const color = document.querySelector(".color")
+button.addEventListener("click", () => {
+  let hexColor = getRandomHexColor()
+   button.style.backgroundColor = getRandomHexColor();
+  color.textContent = getRandomHexColor()
+})
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-
+ 
 }
-document.querySelector(`.change-color`).style.backgroundColor = getRandomHexColor()
