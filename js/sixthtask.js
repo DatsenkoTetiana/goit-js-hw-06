a5b09c81cb1e8fe582479934d5cb5da8e6a9936a
tetianadac <datsenkotetiana027@gmail.com>
2022-10-24 11:36:08 +0300

    add

--- js/sixthtask.js
+++ js/sixthtask.js
@@ -1,28 +1,37 @@
 `use strict`;
 
-const inputVal = document.getElementById("validation-input");
-const totalLenght = inputVal.getAttribute("data-length");
-const intTotallenght = parseInt(totalLenght, 10);
-inputVal.addEventListener(
-  "focus",
-  function (event) {
-    event.target.style.outline = "none";
-  },
-  true
-);
-inputVal.addEventListener("blur", () => {
-  if (inputVal.value.length === intTotallenght) {
-    inputVal.classList.add("valid");
-    inputVal.classList.remove("invalid");
+const inputVal = document.querySelector("#validation-input");
+
+const addRemovePassword = (remove, add) => {
+  inputVal.classList.remove(`${remove}`);
+  inputVal.classList.add(`${add}`);
+};
+
+const validPassword = (e) => {
+  if (Number(inputVal.dataset.length) === e.currentTarget.value.length) {
+    addRemovePassword("invalid", "valid");
+  } else {
+    addRemovePassword("valid", "invalid");
   }
-  if (inputVal.value.length === 0) {
-    inputVal.classList.remove("valid");
-    inputVal.classList.remove("invalid");
-  }
-  if (inputVal.value.length !== intTotallenght && inputVal.value.length !== 0) {
-    inputVal.classList.add("invalid");
-  }
-});
-document
-  .getElementById("validation-input")
-  .setAttribute("style", " height: 50px;");
+};
+
+inputVal.addEventListener("blur", validPassword);
+// const inputVal = document.getElementById("validation-input");
+// const totalLenght = inputVal.getAttribute("data-length");
+// const intTotallenght = parseInt(totalLenght, 10);
+// inputVal.addEventListener("blur", () => {
+//   if (inputVal.value.length === intTotallenght) {
+//     inputVal.classList.add("valid");
+//     inputVal.classList.remove("invalid");
+//   }
+//   if (inputVal.value.length === 0) {
+//     inputVal.classList.remove("valid");
+//     inputVal.classList.remove("invalid");
+//   }
+//   if (inputVal.value.length !== intTotallenght && inputVal.value.length !== 0) {
+//     inputVal.classList.add("invalid");
+//   }
+// });
+// document
+//   .getElementById("validation-input")
+//   .setAttribute("style", " height: 50px;");
